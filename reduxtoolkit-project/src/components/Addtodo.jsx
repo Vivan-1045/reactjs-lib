9d5310b05bf55dd7dcaa144e204ceaf2cdc1398f
Vivek0001 <vivek10452@gmail.com>
2024-07-09 22:37:58 +0530

    commit it

--- reduxtoolkit-project/src/components/Addtodo.jsx
+++ reduxtoolkit-project/src/components/Addtodo.jsx
@@ -27,7 +27,7 @@ function AddTodo() {
       <input
         type="text"
         className="w-auto bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-        placeholder="Enter a Notes..."
+        placeholder="Write Notes..."
         value={input}
         onChange={(e) => setInput(e.target.value)}
       />
